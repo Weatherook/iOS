fd2b3c58a9861c945e295095bdf1c32aac996965
eojin <eozinee@gmail.com>
2018-07-05 02:38:29 +0900

    main view push

---
+++ Weatherook/ui/Main/MainRecommendCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainRecommendCollectionViewCell.swift
+//  Weatherook
+//
+//  Created by 양어진 on 2018. 7. 5..
+//  Copyright © 2018년 이영준. All rights reserved.
+//
+
+import UIKit
+
+class MainRecommendCollectionViewCell: UICollectionViewCell {
+    
+}

d7f9af56e245c75b933424b1bf00033d029b29da
eojin <eozinee@gmail.com>
2018-07-11 23:07:54 +0900

    Add View

---
+++ Weatherook/UserInterface/Filter/FilterContentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilterContentTableViewCell.swift
+//  Weatherook
+//
+//  Created by 양어진 on 2018. 7. 11..
+//  Copyright © 2018년 이영준. All rights reserved.
+//
+
+import UIKit
+
+class FilterContentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
